f33b4dc580ec511a8868f173c1e52ced6dd78f16
aajungroth <aajungroth@ucdavis.edu>
2017-05-19 16:12:59 -0700

    Completed linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    //If we have an empty linked list
+    if (list.head === null) {
+      list.head = node;
+    //If we have at least one element in the linked list
+    } else if (list.tail !== null) {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var next = list.head.next;
+    var head = list.head.value;
+
+    list.head = next;
+    return head;
   };
 
   list.contains = function(target) {
+    if (list.head !== null) {
+      if (list.tail.value === target) {
+        return true;
+      }
+      var currentNode = list.head;
+
+      while (currentNode.next !== null) {
+        if (currentNode.value === target) {
+          return true;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+    return false;
   };
 
   return list;
