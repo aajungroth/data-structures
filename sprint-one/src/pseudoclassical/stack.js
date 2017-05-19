9f83bb68c99de30cf55da857f790d682a49a8a7c
aajungroth <aajungroth@ucdavis.edu>
2017-05-18 21:47:04 -0700

    Passed first test for pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  // size becomes a property of an instance of Stack
+  // this is now bound to that instance, no return required
 };
 
+Stack.prototype.size = function() {
+  return this._size;
+};
 
+// protoype is like stack methods
+// if you make a method inside then all instances would get one
