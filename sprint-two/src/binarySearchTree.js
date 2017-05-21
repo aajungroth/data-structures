cee27967e9a40e2d30f7306433f22e7244855650
aajungroth <aajungroth@ucdavis.edu>
2017-05-21 10:11:53 -0700

    Completed binary search tree insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,37 +13,43 @@ var BinarySearchTree = function(value) {
 
 var storageMethods = {};
 
-storageMethods.insert = function(value) {
-  var next;
-  // right node or left node
-  do {
-    // if not null then...
-    if (value > this.value) {
-      // next
-      next = this.right;
-    } else {
-      next = this.left;
-    }
-  } while (next !== null);
 
-  next = BinarySearchTree(value);
-  // if (value > this.value) {
-  //   this.right = BinarySearchTree(value);
-  // } else {
-  //   this.left = BinarySearchTree(value);
-  // }
+storageMethods.insert = function(value) {
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+
 };
 
+//BinarySearchTree(value);
+// storageMethods.insert = function(value) {
+//   var next;
+//   // right node or left node
+//   do {
+//     // if not null then...
+//     if (value > this.value) {
+//       // next
+//       next = this.right;
+//     } else {
+//       next = this.left;
+//     }
+//   } while (next !== null);
+//
+//   next = BinarySearchTree(value);
+// };
+
 storageMethods.contains = function() {};
-
 storageMethods.depthFirstLog = function() {};
-
-// binarySearchTree.insert(2);
-// binarySearchTree.insert(3);
-// binarySearchTree.insert(7);
-// binarySearchTree.insert(6);
-// expect(binarySearchTree.left.right.value).to.equal(3);
-// expect(binarySearchTree.right.left.value).to.equal(6);
 
 /*
  * Complexity: What is the time complexity of the above functions?
