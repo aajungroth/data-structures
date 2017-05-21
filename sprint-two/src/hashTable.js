1e71a1fd446bf87abe0a558b637b1e1d804bba40
aajungroth <aajungroth@ucdavis.edu>
2017-05-20 17:03:59 -0700

    Completed the hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,21 +6,44 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
 
-  this._storage[index] = v;
+  if (!Array.isArray(this._storage[index])) {
+    this._storage[index] = [];
+  }
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index][i][1] = v;
+    }
+  }
+  this._storage[index].push(tuple);
+  //this._storage[index] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  return this._storage[index];
+  //debugger;
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if ((this._storage[index][i]) && (this._storage[index][i][0] === k)) {
+      return this._storage[index][i][1];
+    }
+  }
+  //return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  delete this._storage[index];
+  //debugger;
+  if (Array.isArray(this._storage[index])) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if ((this._storage[index][i]) && (this._storage[index][i][0] === k)) {
+        delete this._storage[index][i];
+      }
+    }
+  }
+  //delete this._storage[index];
 };
 
 
